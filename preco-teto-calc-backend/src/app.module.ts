e6317185f319b2938769993943eb3e6dba428373
Leandro Gehlen <legedeveloper@gmail.com>
2023-08-29 17:43:12 -0300

    user crud structure and stock crud structure created with nestjs cli

--- preco-teto-calc-backend/src/app.module.ts
+++ preco-teto-calc-backend/src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { SharedModule } from './shared/shared.module';
+import { StocksModule } from './stocks/stocks.module';
+import { UsersModule } from './users/users.module';
+
 @Module({
-  imports: [],
+  imports: [SharedModule, StocksModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
