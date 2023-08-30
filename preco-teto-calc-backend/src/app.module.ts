6e3b99b78307e7e1e01e7d8faba35199b8ebaf59
Leandro Gehlen <legedeveloper@gmail.com>
2023-08-30 07:10:45 -0300

    jwt guard is now working for all application endpoints. Must use @isPublic decorator to make an endpoint public from now on

--- preco-teto-calc-backend/src/app.module.ts
+++ preco-teto-calc-backend/src/app.module.ts
@@ -6,10 +6,18 @@ import { SharedModule } from './shared/shared.module';
 import { StocksModule } from './stocks/stocks.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [SharedModule, StocksModule, UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
