bd4bf4fde10889199a613f0f503eefa4c76ab169
Leandro Gehlen <legedeveloper@gmail.com>
2023-08-31 00:06:22 -0300

    texto página home feito

--- preco-teto-calc-frontend/src/app/app.module.ts
+++ preco-teto-calc-frontend/src/app/app.module.ts
@@ -4,9 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HomeComponent } from './pages/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    NotfoundComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
