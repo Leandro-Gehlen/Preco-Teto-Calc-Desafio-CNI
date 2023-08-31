5348d861a47e20cbc8498f554f2213c65a8d3090
Leandro Gehlen <legedeveloper@gmail.com>
2023-08-31 05:23:32 -0300

    tudo certo com a aplicação e bootstrap instalado.

--- preco-teto-calc-frontend/src/app/app-routing.module.ts
+++ preco-teto-calc-frontend/src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-//import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
-import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
-  {path: '', component:AppComponent},
+  {path: '', component:HomeComponent},
   {path: 'login', component:LoginComponent},
   {path: 'signup', component: SignupComponent},
   {path: '**', component: NotfoundComponent},
